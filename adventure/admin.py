9cd1f658c4e0187589c281cac5c938c290b50323
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-11-19 00:04:12 -0500

    Adds Room & Player models to admin site registration

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Room, Player
+
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Player, PlayerAdmin)
